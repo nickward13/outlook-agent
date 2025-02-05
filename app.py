bc670ee3f0c418ae27dd4fd74483bcab90c39230
Nick Ward <nickward@microsoft.com>
2025-02-05 14:38:08 +1100

    wrapped gradio in fastapi for future oauth authentication

--- app.py
+++ app.py
@@ -1,23 +1,27 @@
 import gradio as gr
 import outlook_agent
-import random
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+CUSTOM_PATH = "/gradio"
+
+app = FastAPI()
+
+@app.get("/")
+def read_main():
+    return RedirectResponse(url=CUSTOM_PATH)
 
 def outlook_agent_chat(message, history):
     new_message = outlook_agent.chat(message)
     return new_message
 
-def random_response(message, history):
-    if len([h for h in history if h['role'] == "assistant"]) % 2 == 0:
-        return f"Yes, I do think that: {message}"
-    else:
-        return f"No, I don't think that: {message}"
-
-def greet(name, intensity):
-    return "Hello, " + name + "!" * int(intensity)
-
-ui = gr.ChatInterface(
+io = gr.ChatInterface(
     fn=outlook_agent_chat,
-    type="messages",
+    type="messages"
 )
 
-ui.launch()
+app = gr.mount_gradio_app(app, io, path=CUSTOM_PATH)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
